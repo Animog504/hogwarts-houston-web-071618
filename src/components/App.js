41bd1fe0d8bad1f7abd11442800b0f60af70c0c2
Flatiron School <flatironschool@Flatirons-MacBook-Air-3.local>
2018-09-20 18:06:48 -0500

    fix the sorts and be done

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,58 @@ import React, { Component } from 'react';
 import '../App.css';
 import Nav from './Nav'
 import hogs from '../porkers_data';
+import PigContainer from './PigContainer';
 
 class App extends Component {
+  state = {
+    allPigs: hogs
+  }
+  toggleGreased=() =>{
+    // this.setState({
+    //   allPigs: something
+    // })
+    let currentlyGreased = !this.state.currentlyGreased
+    let allPigs = hogs
+    console.log("IMA GREASEBOI!")
+    if(currentlyGreased) allPigs = hogs.filter(hog => hog.greased)
+
+    this.setState({
+      currentlyGreased,
+      allPigs
+    })
+  }//setGreased
+  setFilter = (e) => {
+    console.log(e.target.value)
+    let pigs = hogs
+    switch(e.target.value){
+
+    case 'weight':
+      pigs = hogs.sort( (hog1, hog2) => {
+        if(hog1.weight < hog2.weight){
+          return -1
+        } else if(hog1.weight > hog2.weight) {
+          return 1
+        } else {
+          return 0
+        }
+      })
+      break;
+    case 'name':
+
+      break;
+    default:
+      pigs = hogs
+    }//switch
+
+    //setState
+    this.setState({ currentFilter: e.target.value})
+  }//setFilter
+
   render() {
     return (
       <div className="App">
-          < Nav />
-
+          < Nav toggleGreased={this.toggleGreased} setFilter={this.setFilter} currentlyGreased={this.state.currentlyGreased}/>
+          < PigContainer allPigs={this.state.allPigs}/>
       </div>
     )
   }
